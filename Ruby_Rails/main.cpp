b9932056056515d401a65e62e41f19479d3ccc2a
Carlos Spaggiari Roa <carlosspaggiari@carloss-macbook-pro.local>
2015-09-18 17:17:04 -0430

    Initial Commit

---
+++ Ruby_Rails/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ruby_Rails
+//
+//  Created by Carlos Spaggiari Roa on 9/18/15.
+//  Copyright © 2015 ARSC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
